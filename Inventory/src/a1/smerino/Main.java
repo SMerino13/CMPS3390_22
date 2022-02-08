eb9b0c76d95839163d26d01ec3eab879a909702a
smerino <smerino@csub.edu>
2022-02-07 23:20:45 -0800

    Started A2

--- Inventory/src/a1/smerino/Main.java
+++ Inventory/src/a1/smerino/Main.java
@@ -6,23 +6,26 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private static final Random ran = new Random();
+    private static final FoodItems[] foodItems = FoodItems.values();
+    private static final Tools[] tools = Tools.values();
+    private static final ToolUses[] toolUses = ToolUses.values();
+
+
+    // Weapons
+    private static final WeaponTypes[] weaponTypes = WeaponTypes.values();
+    private static final WeaponNames[] weaponNames = WeaponNames.values();
+
+    // Potions
+    private static final Potions[] potionNames = Potions.values();
+    private static final PotionTypes[] potionTypes = PotionTypes.values();
+    private static final PotionPurchases[] potionPurchases = PotionPurchases.values();
+
+
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Random ran = new Random();
         List<Item> items = new ArrayList<>();
-        FoodItems[] foodItems = FoodItems.values();
-        Tools[] tools = Tools.values();
-        ToolUses[] toolUses = ToolUses.values();
-
-        // Weapons
-        WeaponTypes[] weaponTypes = WeaponTypes.values();
-        WeaponNames[] weaponNames = WeaponNames.values();
-
-        // Potions
-        Potions[] potionNames = Potions.values();
-        PotionTypes[] potionTypes = PotionTypes.values();
-        PotionPurchases[] potionPurchases = PotionPurchases.values();
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("How many items do you want: ");
         int itemCnt = Integer.parseInt(scan.nextLine());
@@ -30,51 +33,55 @@ public class Main {
         for(int i=0; i<itemCnt; i++) {
             int type = ran.nextInt(4);
             switch (type) {
-                case 0 -> {
-                    int foodIndex = ran.nextInt(foodItems.length);
-                    String foodName = foodItems[foodIndex].toString();
-                    float foodPrice = ran.nextFloat(10);
-                    int foodQty = ran.nextInt(30);
-                    int foodUses = ran.nextInt(6);
-                    float healthGain = ran.nextFloat(20);
-                    Food tmpFood = new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
-                    items.add(tmpFood);
-                }
-                case 1 -> {
-                    int toolIndex = ran.nextInt(tools.length);
-                    String toolName = tools[toolIndex].toString();
-                    float toolPrice = ran.nextFloat(200);
-                    int toolQty = ran.nextInt(15);
-                    String use = toolUses[toolIndex].toString();
-                    Tool tmpTool = new Tool(toolName, toolPrice, toolQty, use);
-                    items.add(tmpTool);
-                }
-                case 2 -> {
-                    int weaponIndex = ran.nextInt(weaponNames.length);
-                    String weaponName = weaponNames[weaponIndex].toString();
-                    float weaponPrice = ran.nextFloat(1000);
-                    int weaponQty = ran.nextInt(15);
-                    String weaponType = weaponTypes[weaponIndex].toString();
-                    float hitPoint = ran.nextFloat(50);
-                    Weapon tmpWeapon = new Weapon(weaponName, weaponPrice, weaponQty, weaponType, hitPoint);
-                    items.add(tmpWeapon);
-                }
-                case 3 -> {
-                    int potionIndex = ran.nextInt(potionNames.length);
-                    String potionName = potionNames[potionIndex].toString();
-                    float potionPrice = ran.nextFloat(15);
-                    int potionQty = ran.nextInt(15);
-                    String potionType = potionTypes[potionIndex].toString();
-                    String potionPurchase = potionPurchases[potionIndex].toString();
-                    float returnSale = potionPrice / 2;
-                    Potion tmpPotion = new Potion(potionName, potionPrice, potionQty, potionType, potionPurchase, returnSale);
-                    items.add(tmpPotion);
-                }
+                case 0 -> items.add(genFood());
+                case 1 -> items.add(genTool());
+                case 2 -> items.add(genWeapon());
+                case 3 -> items.add(genPotion());
             }
         }
 
         for(Item i : items){
             System.out.println(i);
         }
+    }
+
+    public static Food genFood(){
+        int foodIndex = ran.nextInt(foodItems.length);
+        String foodName = foodItems[foodIndex].toString();
+        float foodPrice = ran.nextFloat(10);
+        int foodQty = ran.nextInt(30);
+        int foodUses = ran.nextInt(6);
+        float healthGain = ran.nextFloat(20);
+        return new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
+    }
+
+    public static Tool genTool(){
+        int toolIndex = ran.nextInt(tools.length);
+        String toolName = tools[toolIndex].toString();
+        float toolPrice = ran.nextFloat(200);
+        int toolQty = ran.nextInt(15);
+        String use = toolUses[toolIndex].toString();
+        return new Tool(toolName, toolPrice, toolQty, use);
+    }
+
+    public static Weapon genWeapon(){
+        int weaponIndex = ran.nextInt(weaponNames.length);
+        String weaponName = weaponNames[weaponIndex].toString();
+        float weaponPrice = ran.nextFloat(1000);
+        int weaponQty = ran.nextInt(15);
+        String weaponType = weaponTypes[weaponIndex].toString();
+        float hitPoint = ran.nextFloat(50);
+        return new Weapon(weaponName, weaponPrice, weaponQty, weaponType, hitPoint);
+    }
+
+    public static Potion genPotion(){
+        int potionIndex = ran.nextInt(potionNames.length);
+        String potionName = potionNames[potionIndex].toString();
+        float potionPrice = ran.nextFloat(15);
+        int potionQty = ran.nextInt(15);
+        String potionType = potionTypes[potionIndex].toString();
+        String potionPurchase = potionPurchases[potionIndex].toString();
+        float returnSale = potionPrice / 2;
+        return new Potion(potionName, potionPrice, potionQty, potionType, potionPurchase, returnSale);
     }
 }
---
+++ MultiThreadSort/src/a2/smerino/Main.java
@@ -0,0 +1,12 @@
+package a2.smerino;
+
+import a1.smerino.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Food f = a1.smerino.Main.genFood();
+        System.out.println(f);
+    }
+}
