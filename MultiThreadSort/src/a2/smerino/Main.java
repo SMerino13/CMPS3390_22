63c60a80b1922d567355063f0554347a1fd94996
smerino <smerino@csub.edu>
2022-02-09 19:15:51 -0800

    a2 Lab Done

--- MultiThreadSort/src/a2/smerino/Main.java
+++ MultiThreadSort/src/a2/smerino/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Random ran = new Random();
 
-        System.out.print("Do you want a [S]ingle Sort or a [D]ual Sort? ");
+        System.out.print("Do you want a [S]ingle Sort or a [D]ual Sort or [Q]uad Sort? ");
         char selection = scan.next().charAt(0);
 
         System.out.print("How many items do you want to sort? ");
@@ -33,7 +33,53 @@ public class Main {
         switch (selection) {
             case 's', 'S' -> SingleSort(items);
             case 'd', 'D' -> DualSort(items);
+            case 'q', 'Q' -> QuadSort(items);
         }
+
+    }
+
+    private static void QuadSort(Item[] items) throws InterruptedException {
+        int mid = Math.round(items.length / 2f);
+        int qrt = Math.round(mid / 2f);
+        int threeQtr = qrt + mid;
+
+        ThreadSort t1 = new ThreadSort(items, 0, qrt);
+        ThreadSort t2 = new ThreadSort(items, qrt, mid);
+        ThreadSort t3 = new ThreadSort(items, mid, threeQtr);
+        ThreadSort t4 = new ThreadSort(items, threeQtr, items.length);
+
+        long startTime = System.nanoTime();
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+
+        MergeSort m1 = new MergeSort(t1.gettItems(), t2.gettItems());
+        MergeSort m2 = new MergeSort(t3.gettItems(), t4.gettItems());
+
+        m1.start();
+        m2.start();
+        m1.join();
+        m2.join();
+
+        MergeSort m3 = new MergeSort(m1.getSortedItems(), m2.getSortedItems());
+
+        m3.start();
+        m3.join();
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime) / 1000000;
+
+        for(Item i : m3.getSortedItems()){
+            System.out.println(i);
+        }
+        System.out.println("Quad sort took: " + duration);
+
 
     }
 
